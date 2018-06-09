1436fb409a20725e5fd6e5d382851b407a918219
Daniel O'Grady <danielogrady@gmx.de>
2018-06-09 14:40:32 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Aschenputtel',
+      version='0.1',
+      description='Bot to count occurrences of emojis.',
+      url='',
+      author='Len',
+      author_email='',
+      license='MIT',
+      packages=['asschenputtel'],
+      install_requires=[
+          'discord',
+          'requests'
+      ],
+      zip_safe=False)
